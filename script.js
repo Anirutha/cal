716d057872f4f1b984ba82dcd78d2aaddfc4e370
Anirutha <107671900+Anirutha@users.noreply.github.com>
2023-03-25 12:08:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,90 @@
-    var table=document.createElement("table");
-    table.setAttribute("id","calcu");
-    console.log(table);
-    
-    function row(tagname){
-        var row=document.createElement(tagname);
-        return row;
-    }
-    
-    var row1=row("tr");
-    var td1=td("td","colspan","3");
-    var input=inputs("input","class","displaybox","type","text","id","result","disabled");
-    var td2=tds("td");
-    var input1=inputs("input","type","button","value","C","click","clearScreen","id","btn");
-    var row2=row("tr");
-    var td3=tds("td");
-    var input2=inputs("input","type","button","value","1","click","display('1')");
-    
-    
-    function td(tagname,attrname,attrvalue){
-        var td=document.createElement(tagname);
-        td.setAttribute(attrname,attrvalue);
-        return td;
-    }
+var div1 = create('div', 'container-fluid mt-5 ml-5 p-5');
 
-    function tds(tagname){
-        var tds=document.createElement(tagname);
-        return tds;
-    }
+let bg = document.querySelector('body');
 
 
-    function inputs(tagname,attrname,attrvalue,attrname1,attrvalue1,attrname2,attrvalue2,attrname3,attrvalue3){
-        var ele1=document.createElement(tagname);
-        ele1.setAttribute(attrname,attrvalue);
-        ele1.setAttribute(attrname1,attrvalue1);
-        ele1.setAttribute(attrname2,attrvalue2);
-        ele1.setAttribute(attrname3,attrvalue3);
-        return ele1;
-    }
-   
-    td1.appendChild(input);
-    row1.appendChild(td1);
-    table.appendChild(row1);
-    
-    td2.appendChild(input2);
-    row2.appendChild(td2);
-    table.appendChild(row2);
-    document.body.append(table);
-    
 
-    <--https://www.makeuseof.com/build-a-simple-calculator-using-html-css-javascript/-->
+var divarr = createElements('div', 'offset-md-2 offset-sm-1 row text-md-center text-sm-left h4 mb-0', 6);
+
+var input = create('input', 'col-8 border-3 border-light text-right mt-5 mb-1');
+input.setAttribute('type', 'text');
+input.disabled = true;
+input.setAttribute('style', `line-height:60px; background-color:#DAE0E2; font-family: 'Comfortaa', cursive;`);
+input.id = "display";
+
+var btnnumbers = createElements('button', 'btn-fluid btn-light text-md-center text-sm-left text-xs-left p-md-3 p-sm-4 p-xs-4 col-2', 10);
+btnnumbers[0].setAttribute('onclick', "appendNumber(0)");
+btnnumbers[1].setAttribute('onclick', `appendNumber(1)`);
+btnnumbers[2].setAttribute('onclick', `appendNumber(2)`);
+btnnumbers[3].setAttribute('onclick', `appendNumber(3)`);
+btnnumbers[4].setAttribute('onclick', `appendNumber(4)`);
+btnnumbers[5].setAttribute('onclick', `appendNumber(5)`);
+btnnumbers[6].setAttribute('onclick', `appendNumber(6)`);
+btnnumbers[7].setAttribute('onclick', `appendNumber(7)`);
+btnnumbers[8].setAttribute('onclick', `appendNumber(8)`);
+btnnumbers[9].setAttribute('onclick', `appendNumber(9)`);
+
+var add = createButton('+', '+', `appendNumber('+')`);
+var sub = createButton('-', '-', `appendNumber('-')`);
+var mul = createButton('x', 'X', `appendNumber('*')`);
+var division = createButton('&#247;', '/', `appendNumber('/')`);
+var modulus=createButton('%','%',`appendNumber('%')`);
+var equals = createButton('=', '=', 'equate()');
+
+var dot = createButton('.', '.', `appendNumber('.')`);
+var clear = createButton('c', 'c', 'clearScreen()');
+
+
+document.body.append(div1);
+div1.append(divarr[0], divarr[1], divarr[2], divarr[3], divarr[4], divarr[5]);
+divarr[0].append(input);
+divarr[1].append(clear);
+divarr[2].append(btnnumbers[7], btnnumbers[8], btnnumbers[9], mul);
+divarr[3].append(btnnumbers[4], btnnumbers[5], btnnumbers[6], sub);
+divarr[4].append(btnnumbers[1], btnnumbers[2], btnnumbers[3], add);
+divarr[5].append(modulus, btnnumbers[0],equals,division);
+
+function create(element, classname) {
+    var t = document.createElement(element);
+    t.setAttribute('class', classname);
+    return t;
+}
+
+function createElements(element, classname, num) {
+    let elem = [];
+    for (let i = 0; i < num; i++) {
+        var b = document.createElement(element);
+        b.setAttribute('class', classname);
+        b.setAttribute('style', `font-family: 'Comfortaa', cursive;`);
+        if (element === 'button') {
+            b.setAttribute('value', i);
+            b.innerHTML = i;
+        }
+        elem.push(b);
+    }
+    return elem;
+}
+
+function createButton(txt, val, fn) {
+    let a = document.createElement('button');
+    a.setAttribute('class', 'btn-fluid btn-light text-md-center text-sm-left text-xs-left p-md-3 p-sm-4 p-xs-4 col-2');
+    a.setAttribute('value', val);
+    a.innerHTML = txt;
+    a.setAttribute('onclick', fn);
+    return a;
+}
+
+
+function appendNumber(val) {
+    input.value += val;
+}
+
+
+function equate() {
+    input.value = Number.isInteger(eval(input.value)) ? eval(input.value) : eval(input.value).toFixed(2);
+}
+
+
+function clearScreen() {
+    input.value = "";
+}
